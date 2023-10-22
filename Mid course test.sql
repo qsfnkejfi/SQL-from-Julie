d6835afa96fde11bb8c7961a42b718ff5a00a04a
Sơn Tùng <132970607+qsfnkejfi@users.noreply.github.com>
2023-10-22 22:02:33 +0700

    Update Mid course test.sql

--- Mid course test.sql
+++ Mid course test.sql
@@ -3,13 +3,11 @@ select min(replacement_cost)
 from film
 -- Exercise 2
 select
-case
+sum(case
 	when replacement_cost between 9.99 and 19.99 then 1
 	else 0
-end as low,
-count(*)
+end) as low
 from film
-group by low
 -- Exercise 3
 select a.title, a.length, c.name
 from film as a
@@ -19,26 +17,33 @@ join category as c
 on b.category_id=c.category_id
 where c.name in('Drama','Sports')
 order by a.length desc
+limit 1
 -- Exercise 4
+select c.name, count(a.title)
 from film as a
-join film_category as b
+inner join film_category as b
 on a.film_id=b.film_id
-join category as c
+inner join category as c
 on b.category_id=c.category_id
+group by c.name
+order by count(a.title) desc
+limit 1
 -- Exercise 5
 select c.first_name ||' '|| c.last_name, count(a.film_id)
 from film as a
-join film_actor as b
+inner join film_actor as b
 on a.film_id=b.film_id
-join actor as c
+inner join actor as c
 on b.actor_id=c.actor_id
 group by c.first_name ||' '|| c.last_name
-order by count(a.film_id) desc
+order by count(a.film_id) desc 
+limit 1
 -- Exercise 6
-select a.address, a.address2
+select count(*) as dia_chi
 from address as a
-left join customer as b
-on a.address_id=b.address_id and b.address_id is null
+full join customer as b
+on a.address_id=b.address_id 
+where b.address_id is null
 -- Exercise 7
 select a.city, sum(d.amount)
 from city as a
@@ -50,6 +55,7 @@ join payment as d
 on c.customer_id=d.customer_id
 group by a.city
 order by sum(d.amount) desc
+limit 1
 -- Exercise 8
 select concat(a.city,', ',o.country) as thanh_pho, sum(d.amount)
 from country as o
@@ -63,3 +69,4 @@ join payment as d
 on c.customer_id=d.customer_id
 group by concat(a.city,', ',o.country)
 order by sum(d.amount) asc
+limit 1
