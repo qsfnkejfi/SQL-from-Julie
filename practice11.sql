92d5aac95881969701b1b105ca359935a7b5c9f6
Sơn Tùng <132970607+qsfnkejfi@users.noreply.github.com>
2023-11-08 03:07:18 +0700

    Update practice11.sql

--- practice11.sql
+++ practice11.sql
@@ -1,3 +1,4 @@
+-- Exercise 1
 create view vw_ecommerce_analyst as(
 with cte as 
 (select Extract(year from created_at) as year, Extract(month from created_at) as month, 
@@ -24,3 +25,47 @@ lead(TPO) over(partition by category order by year,month ) as next_order,
 round(total_profit/total_cost,2) as profit_to_cost_ratio
 from cte)
 )
+
+--Exercise 2:
+with cte2 as(
+select product_id, 
+product_retail_price,
+extract(year from created_at) ||'-' || extract(month from created_at) as cohort_date,
+created_at,
+(extract(year from created_at)-extract(year from first_purchase_date))*12
++ extract(month from created_at)-extract(month from first_purchase_date) +1 as index
+
+from(
+select product_id,
+product_retail_price,
+min(created_at) over (partition by product_id) as first_purchase_date,
+created_at
+from bigquery-public-data.thelook_ecommerce.inventory_items)
+order by extract(year from created_at), extract(month from created_at)
+)
+,cte3 as(
+select cohort_date,
+index,
+count(distinct product_id) as cnt,
+sum(product_retail_price) as revenue
+from cte2
+group by 1,2
+)
+
+, product_cohort as(
+select cohort_date,
+sum(case when index =1 then cnt else 0 end) as m1,
+sum(case when index =2 then cnt else 0 end) as m2,
+sum(case when index =3 then cnt else 0 end) as m3,
+sum(case when index =4 then cnt else 0 end) as m4,
+from cte3
+group by 1
+order by 1
+)
+
+select cohort_date,
+round(100.00* m1/nullif(m1,0) ,2) as m1,
+round(100.00* m2/nullif(m1,0) ,2) as m2,
+round(100.00* m3/nullif(m1,0) ,2) as m3,
+round(100.00* m4/nullif(m1,0) ,2) as m4
+from product_cohort
